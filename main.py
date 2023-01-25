2677f7e47defe5558d8f9e5fd59038778e92d2f9
FsPilots <Alexandregasse.hellio@gmail.com>
2023-01-26 00:23:52 +0100

    Update

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import cv2 as cv
 import video as vi
 import app as a
 import facialdetection as f1
-import image_recognition.window_detection_live as wdl
 import tkinter
 import PIL.Image, PIL.ImageTk
 import time
